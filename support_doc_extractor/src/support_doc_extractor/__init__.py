0d0d3bbf549a499b59b423703dd1fc6181aed471
zijie <31406021+1524701427@users.noreply.github.com>
2026-09-22 16:40:51 +0800

    feat: export SupportDocType enum

--- support_doc_extractor/src/support_doc_extractor/__init__.py
+++ support_doc_extractor/src/support_doc_extractor/__init__.py
@@ -3,6 +3,7 @@
 from support_doc_extractor.engine import SupportDocPipeline, extract_document
 from support_doc_extractor.models import Block, Document, ExtractedField, ExtractionResult, Page, Table
 from support_doc_extractor.logging_utils import configure_logging
+from support_doc_extractor.document_types import SupportDocType
 
 __all__ = [
     "Block",
@@ -14,4 +15,5 @@ __all__ = [
     "SupportDocPipeline",
     "extract_document",
     "configure_logging",
+    "SupportDocType",
 ]
